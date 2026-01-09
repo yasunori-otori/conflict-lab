3f60c2427ed41e4db314f321e0d7a3af0395286d
yasunori-otori <yasunori.otori@d2cr.co.jp>
2026-01-09 09:52:12 +0900

    リセット

--- main.go
+++ main.go
@@ -2,20 +2,8 @@ package main
 
 import "fmt"
 
-func mainT() {
+func main() {
 	fmt.Println("a")
-	fmt.Println("t")
+	fmt.Println("b")
 	fmt.Println("c")
-}
-
-func FuncA() {
-	fmt.Println("Func A")
-}
-
-func FuncB() {
-	fmt.Println("Func B")
-}
-
-func FuncC() {
-	fmt.Println("Func C")
 }
